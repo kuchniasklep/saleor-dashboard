75f24d79f28bde0962bf373e9b5c5f1761f4905c
Hazel Szoka <development@hazelcode.eu>
2026-03-25 12:17:49 +0100

    Prefix editorjs endpoints with vite

--- src/components/RichTextEditor/consts.ts
+++ src/components/RichTextEditor/consts.ts
@@ -48,8 +48,8 @@ export const tools = (): Record<string, ToolConstructable | ToolSettings> => ({
     class: ImageTool,
     config: {
       endpoints: {
-        byFile: process.env.EDITORJS_IMAGE_BY_FILE_ENDPOINT,
-        byUrl: process.env.EDITORJS_IMAGE_BY_URL_ENDPOINT,
+        byFile: process.env.VITE_EDITORJS_IMAGE_BY_FILE_ENDPOINT,
+        byUrl: process.env.VITE_EDITORJS_IMAGE_BY_URL_ENDPOINT,
       },
       additionalRequestHeaders: {
         Authorization: `Bearer ${storage.getAccessToken()}`,
